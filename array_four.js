c1baf8ab281937f425ebcf333ec9ab5292580a67
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-02-25 07:27:06 +0000

    changing file

--- array_four.js
+++ array_four.js
@@ -1,4 +1,5 @@
 let arr=[1,2,3,4,5,6,7];
 // console.log(marks.slice(2,5));
-arr.splice(2,3,101,102,103);
-console.log(arr);
+// arr.splice(2,3,101,102,103);
+// console.log(arr);
+console.log(arr.splice(3));
